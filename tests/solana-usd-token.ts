e0350102a27c9ff0d5eb85a5cc2e63900f9256be
congmu <congmu@congmu.com>
2025-08-19 15:22:29 +0800

    add test and gitconfig

--- tests/solana-usd-token.ts
+++ tests/solana-usd-token.ts
@@ -1,16 +1,83 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { SolanaUsdToken } from "../target/types/solana_usd_token";
+import { Keypair } from '@solana/web3.js';
 
-describe("solana-usd-token", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('Transfer Tokens', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.SolanaUsdToken as anchor.Program<SolanaUsdToken>;
 
-  const program = anchor.workspace.solanaUsdToken as Program<SolanaUsdToken>;
+  const metadata = {
+    name: 'Solana Usdt',
+    symbol: 'USDT',
+    uri: 'https://raw.githubusercontent.com/congmucc/solana-usd-token/main/public/spl-token.json',
+  };
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // Generate new keypair to use as address for mint account.
+  const mintKeypair = new Keypair();
+
+  // Generate new keypair to use as address for recipient wallet.
+  const recipient = new Keypair();
+
+  // Derive the associated token address account for the mint and payer.
+  const senderTokenAddress = getAssociatedTokenAddressSync(mintKeypair.publicKey, payer.publicKey);
+
+  // Derive the associated token address account for the mint and recipient.
+  const recepientTokenAddress = getAssociatedTokenAddressSync(mintKeypair.publicKey, recipient.publicKey);
+
+  it('Create an SPL Token!', async () => {
+    const transactionSignature = await program.methods
+      .createToken(metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      })
+      .signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+  });
+
+  it('Mint tokens!', async () => {
+    // Amount of tokens to mint.
+    const amount = new anchor.BN(100);
+
+    // Mint the tokens to the associated token account.
+    const transactionSignature = await program.methods
+      .mintToken(amount)
+      .accounts({
+        mintAuthority: payer.publicKey,
+        recipient: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+        associatedTokenAccount: senderTokenAddress,
+      })
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Associated Token Account Address: ${senderTokenAddress}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+  });
+
+  it('Transfer tokens!', async () => {
+    // Amount of tokens to transfer.
+    const amount = new anchor.BN(50);
+
+    const transactionSignature = await program.methods
+      .transferTokens(amount)
+      .accounts({
+        sender: payer.publicKey,
+        recipient: recipient.publicKey,
+        mintAccount: mintKeypair.publicKey,
+        senderTokenAccount: senderTokenAddress,
+        recipientTokenAccount: recepientTokenAddress,
+      })
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Transaction Signature: ${transactionSignature}`);
   });
 });
